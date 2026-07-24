d0f45711d464e7f52afd684050519d8d0704d7e4
balathan2004 <balathan2004@gmail.com>
2026-07-24 09:05:03 +0530

    redux implemented and controller and services added

--- components/elements/groupedBox.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import { docInterface } from "../utils/interfaces";
-
-interface Props {
-  data: docInterface[];
-  name: string;
-}
-
-export default function GroupedBox({ data, name }: Props) {}
